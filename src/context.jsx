f6816c7abaf50e06a5ca8bf5cc1ed8212054895e
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-27 19:06:43 +0700

    Add state for dark theme

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,19 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const AppContent = createContext();
 
 export const AppProvider = ({ children }) => {
-  <AppContent.Provider>{children}</AppContent.Provider>;
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  return (
+    <AppContent.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContent.Provider>
+  );
 };
 
 export const useGlobalContext = () => useContext(AppContent);
