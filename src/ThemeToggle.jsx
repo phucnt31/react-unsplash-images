5ba2a8aabf9012ef20732589ca0b8ad986ac9255
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-27 19:11:12 +0700

    Customize ThemeToggle.jsx

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+import { useGlobalContext } from "./context";
 
 const ThemeToggle = () => {
-  return <div>ThemeToggle</div>;
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+
+  return (
+    <section className="toggle-container">
+      <button type="button" className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillSunFill className="toggle-icon" />
+        ) : (
+          <BsFillMoonFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 
 export default ThemeToggle;
