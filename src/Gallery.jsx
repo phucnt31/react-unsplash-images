28652eaea92cf0943207b4838f994bb1116f0e90
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-27 20:08:59 +0700

    Create .env to store API_KEY and setup react query in Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const Gallery = () => {
+  const resp = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=cat`);
+      return result.data;
+    },
+  });
+  console.log(resp);
   return <div>Gallery</div>;
 };
 
