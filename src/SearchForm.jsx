2d93af5cf0e368e1762df0dfb904d4e0623da102
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-27 19:52:23 +0700

    Customize SearchForm.jsx

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.elements.search.value);
+  };
+
+  return (
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="search-input form-input"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
